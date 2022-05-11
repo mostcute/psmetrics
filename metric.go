dfc938231c8df60694dd62958dc4b9430a5a0949
cici <18758853747@163.com>
2022-05-11 20:43:10 +0800

    fix path

--- metric.go
+++ metric.go
@@ -27,7 +27,7 @@ func HandleDiskUsage(c *gin.Context) {
 	path := c.GetHeader("path")
 	stats, err := psDisk.Usage(path)
 	if err != nil {
-		log.Println("err", err)
+		log.Println("err", err, path)
 	}
 	c.JSON(http.StatusOK, stats)
 }
